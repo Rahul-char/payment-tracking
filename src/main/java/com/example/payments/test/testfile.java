83b1828700345ba09f8519a01c9d629cdd1bb352
Rahul <rahul.kasoju@gmail.com>
2024-10-17 12:50:24 +0530

    test2

--- src/main/java/com/example/payments/test/testfile.java
+++ src/main/java/com/example/payments/test/testfile.java
@@ -2,4 +2,9 @@ package com.example.payments.test;
 
 public class testfile {
 
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
 }
