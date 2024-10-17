008d417ff873a15b79ba13070660c7c0adca71f1
Rahul <rahul.kasoju@gmail.com>
2024-10-17 14:34:52 +0530

    test3

--- src/main/java/com/example/payments/test/testfile.java
+++ src/main/java/com/example/payments/test/testfile.java
@@ -4,7 +4,5 @@ public class testfile {
 
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
     }
 }
